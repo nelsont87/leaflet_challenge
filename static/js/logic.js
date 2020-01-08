43b4226f559f77b747b8ea17afce695ef015a0e0
nelsont87 <nelsont87@gmail.com>
2020-01-08 10:52:39 -0800

    finish part 1

---
+++ static/js/logic.js
@@ -0,0 +1,97 @@
+
+var apiKey = "pk.eyJ1IjoiZGwyYWcwMTciLCJhIjoiY2szcW1nYmxuMDM3ZzNjbjNoOWw2aHlvNiJ9.PItRLUQcpkUVj_WFkJQMtg";
+
+var earthquake = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: apiKey
+});
+
+var map = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 4
+});
+
+
+earthquake.addTo(map);
+
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "black",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  function getColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+      return "red";
+    case magnitude > 4:
+      return "orange";
+    case magnitude > 3:
+      return "lightorange";
+    case magnitude > 2:
+      return "yellow";
+    case magnitude > 1:
+      return "lightgreen";
+    default:
+      return "green";
+    }
+  }
+
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(map);
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "red",
+      "orange",
+      "lightorange",
+      "yellow",
+      "lightgreen",
+      "green"
+    ];
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(map);
+});
